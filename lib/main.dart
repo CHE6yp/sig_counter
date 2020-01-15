e70553a72557ff478d98f350e3aecdc6a3e9fa65
CHE6yp <che6yp@gmail.com>
2020-01-15 22:40:00 +0300

    init

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import 'package:flutter/material.dart';
+import "package:http/http.dart";
+import 'package:intl/date_symbol_data_local.dart';
+import 'config_data.dart';
+
+
+void main() => runApp(Counter());
+
+class Counter extends StatelessWidget{
+
+  static Client httpClient;
+//  static var config;
+
+  Counter(){
+    httpClient = Client();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        title: 'Счетчик сижек',
+
+        home:Scaffold(
+            backgroundColor: Color.fromARGB(255, 255, 255, 100),
+            appBar: AppBar(
+              title: Text('Счетчик сижек'),backgroundColor: Color.fromARGB(255, 100, 100, 255),
+            ),
+            body:Center(
+                child: Day()
+            )
+        )
+    );
+  }
+
+
+}
+
+class Day extends StatefulWidget{
+  final int s =0;
+  @override
+  State<StatefulWidget> createState() {
+    return DayState();
+  }
+}
+
+class DayState extends State<Day>{
+  int y;
+  int m;
+  int d;
+  int sigCount;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    setState(() {
+      fetchData();
+    });
+    return Column(
+      children: <Widget>[
+        Column(
+          children: [
+//              Text("$context"),
+            Text("Сижек за $d $m $y:", style: TextStyle(fontSize: 16,fontStyle: FontStyle.italic),),
+//              Text("Сижек за "+initializeDateFormatting("ru_RU",null).then(()=>{new DateFormat.yMMMd().format(dateTime)})),
+            Text("$sigCount", style: TextStyle(fontSize: 62, color: (sigCount<5)?Colors.green:Colors.red),),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            FloatingActionButton(child: Text("-"), onPressed: ()=>{ setState(()=> {changeSigCount(-1)})},),
+            FloatingActionButton(child: Text("+"), onPressed: ()=>{ setState(()=> {changeSigCount(1)}) },)
+          ],
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        )
+      ],
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+    );
+  }
+
+  changeSigCount(int x) async {
+    print ('cj');
+    sigCount+=x;
+    await Counter.httpClient.get(configData['HOSTNAME']+'/calendar/saveDay/$y/$m/$d/$sigCount');
+  }
+
+  fetchData() async {
+    //Client httpClient = Client();
+    DateTime dateTime = DateTime.now();
+    y = dateTime.year;
+    m = dateTime.month;
+    d = dateTime.day;
+    var response = await Counter.httpClient.get(configData['HOSTNAME']+'/calendar/getDay/$y/$m/$d');
+
+    print("getDay "+response.body);
+    sigCount = int.parse(response.body);
+  }
+}
