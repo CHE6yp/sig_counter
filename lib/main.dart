cd0d38afbcf78aa698fccca52a214f71f73e3e87
CHE6yp <che6yp@gmail.com>
2020-01-18 20:01:23 +0300

    Better loading circle, date picker

--- lib/main.dart
+++ lib/main.dart
@@ -3,45 +3,42 @@
 // found in the LICENSE file.
 import 'package:flutter/material.dart';
 import "package:http/http.dart";
-import 'package:intl/date_symbol_data_local.dart';
 import 'config_data.dart';
-
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 void main() => runApp(Counter());
 
 class Counter extends StatelessWidget{
 
   static Client httpClient;
-//  static var config;
 
   Counter(){
     httpClient = Client();
   }
 
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-        title: 'Счетчик сижек',
+      title: 'Счетчик сижек',
 
-        home:Scaffold(
-            backgroundColor: Color.fromARGB(255, 255, 255, 100),
-            appBar: AppBar(
-              title: Text('Счетчик сижек'),backgroundColor: Color.fromARGB(255, 100, 100, 255),
-            ),
-            body:Center(
-                child: Day()
-            )
+      home:Scaffold(
+        //backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Счетчик сижек')//backgroundColor: Color.fromARGB(255, 100, 100, 255),
+
+        ),
+        body:Center(
+          child: Day()
         )
+      ),
+      theme: ThemeData(primaryColor: Color.fromARGB(255, 100, 100, 255)),
     );
   }
-
-
 }
 
 class Day extends StatefulWidget{
-  final int s =0;
+  final int s = 0;
   @override
   State<StatefulWidget> createState() {
     return DayState();
@@ -49,40 +46,56 @@ class Day extends StatefulWidget{
 }
 
 class DayState extends State<Day>{
+  DateTime pickedDate;
   int y = 0;
   int m = 0;
   int d = 0;
   int sigCount = 0;
-  bool isLoading = false;
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    isLoading = true;
-//    _loadData();
-//    fetchData();
+    changeDate(DateTime.now());
+    //fetchData();
   }
 
-
+  //like Update() in Unity
   @override
   Widget build(BuildContext context) {
-    setState(() {
-      fetchData();
-    });
-    return (isLoading)? Center(child: CircularProgressIndicator(),): Column(
+    return Column(
       children: <Widget>[
         Column(
           children: [
-//              Text("$context"),
-            Text("Сижек за $d $m $y:", style: TextStyle(fontSize: 16,fontStyle: FontStyle.italic),),
-//              Text("Сижек за "+initializeDateFormatting("ru_RU",null).then(()=>{new DateFormat.yMMMd().format(dateTime)})),
-            Text("$sigCount", style: TextStyle(fontSize: 62, color: (sigCount<5)?Colors.green:Colors.red),),
+            FlatButton(child:Text("Сижек за $d $m $y:", style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic)),
+              onPressed: (){
+                DatePicker.showDatePicker(
+                  context,
+                  showTitleActions: true,
+                  minTime: DateTime(2019, 10, 5),
+                  maxTime: DateTime.now(),
+                  onChanged: (date) {
+                    print('change $date');
+                  },
+                  onConfirm: (date) {
+                    print('confirm $date');
+                    changeDate(date);
+                  },
+                  currentTime: pickedDate,
+                  locale: LocaleType.ru
+                );
+              },
+            ),
+            Container(
+              child: (isLoading)? Center(child: CircularProgressIndicator(),heightFactor: 2.55,):
+                Text("$sigCount", style: TextStyle(fontSize: 78, color: (sigCount<5)? Colors.green : Colors.red),)
+            )
           ],
         ),
         Row(
           children: <Widget>[
-            FloatingActionButton(child: Text("-"), onPressed: ()=>{ setState(()=> {changeSigCount(-1)})},),
-            FloatingActionButton(child: Text("+"), onPressed: ()=>{ setState(()=> {changeSigCount(1)}) },)
+            FloatingActionButton(child: Text("-"), backgroundColor: Theme.of(context).primaryColor, onPressed: ()=>changeSigCount(-1)),
+            FloatingActionButton(child: Text("+"), backgroundColor: Theme.of(context).primaryColor, onPressed: ()=>changeSigCount(1))
           ],
           mainAxisAlignment: MainAxisAlignment.spaceAround,
         )
@@ -92,25 +105,32 @@ class DayState extends State<Day>{
   }
 
   changeSigCount(int x) async {
-    print ('cj');
+    print ('count sig $x');
     sigCount+=x;
     await Counter.httpClient.get(configData['HOSTNAME']+'/calendar/saveDay/$y/$m/$d/$sigCount');
+    fetchData();
+  }
+
+  changeDate(DateTime date){
+    setState((){
+      pickedDate = date;
+      y = pickedDate.year;
+      m = pickedDate.month;
+      d = pickedDate.day;
+    });
+    fetchData();
   }
 
   fetchData() async {
-    //Client httpClient = Client();
-    DateTime dateTime = DateTime.now();
-    y = dateTime.year;
-    m = dateTime.month;
-    d = dateTime.day;
-//    isLoading = true;
+    setState((){isLoading = true;});
+    print ("isLoading before $isLoading");
     var response = await Counter.httpClient.get(configData['HOSTNAME']+'/calendar/getDay/$y/$m/$d');
 
     setState(() {
       isLoading = false;
+      sigCount = int.parse(response.body);
     });
 
-    print("getDay "+response.body);
-    sigCount = int.parse(response.body);
+    print ("isLoading after $isLoading");
   }
 }
